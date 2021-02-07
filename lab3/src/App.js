036082ed0d9b1abe1aa432e9804de137e00ea114
“veruskarivera” <veruska@knights.ucf.edu>
2021-02-06 19:14:18 -0500

    lab3

---
+++ lab3/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import HomePage from './components/HomePage';
+
+
+ 
+function App() {
+  return (
+    <div className="App">
+      <HomePage firstName='Veruska Rivera-González :)' />
+    </div>
+  );
+}
+
+export default App;
