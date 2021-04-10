472b2ccbee93a70db648b73e470d8b808122fd27
“veruskarivera” <veruska@knights.ucf.edu>
2021-04-10 11:00:30 -0400

    lab11

--- lab11/database.js
+++ lab11/database.js
@@ -10,38 +10,87 @@ class Database {
     this.collection = null;
   }
 
-  async connect(database, collection) {
-    this.connection = await MongoClient.connect(url);
-    this.database = this.connection.db(database);
-    this.collection = this.database.collection(collection);
+  async connect(lab11, books) {
+    this.connection = await MongoClient.connect(url, {
+      useUnifiedTopology: true
+    });
+    this.database = this.connection.db(lab11);
+    this.collection = this.database.collection(books);
   }
 
-  close() {}
-
-  createOne() {}
-
-  readOne() {}
-
-  readMany() {}
-
-  async updateOne(isbn, title, author, description) {
+  //PUT (App.put() )-> Database.createOne() -> collection.insertOne()
+  async createOne(ISBN, title, author, description) {
     if (this.collection != null) {
-      const result = await this.collection.updateOne(
-        { isbn: isbn },
-        { $set: { title: title } }
-      );
-      return;
+      const result = await this.collection.insertOne({
+        ISBN: ISBN,
+        title: title,
+        author: author,
+        description: description
+      });
+
+      return {
+        ISBN: ISBN,
+        title: title,
+        author: author,
+        description: description
+      };
     } else {
       return null;
     }
   }
 
-  async deleteOne(isbn) {
+  //GET (App.get() )-> Database.readOne() -> collection.findOne()
+  async readOne(ISBN) {
     if (this.collection != null) {
-      const result = await this.collection.deleteOne({ isbn: isbn });
+      const result = await this.collection.findOne({
+        ISBN: ISBN
+      });
+
+      return { result };
+    } else {
+      return { book: "not found" };
+      //not returning what i want, but kinda works
+    }
+  }
+
+  //POST  (App.post()) -> Database.readMany() -> collection.readMany()
+  async readMany(title, author) {
+    if (this.collection != null) {
+      const result = await this.collection.find({
+        title: title,
+        author: author
+      });
+    }
+    //returns an empty paramater, idk why tried everything. also disconnects me from server?
+    return { book: result };
+  }
+
+  //PATCH(App.patch() )-> Database.updateOne() -> collection.updateOne()
+  async updateOne(ISBN, title, author, description) {
+    if (this.collection != null) {
+      const result = await this.collection.updateOne(
+        { ISBN: ISBN },
+        { $set: { title: title, author: author, description: description } }
+      );
+      return { title: title, author: author, description: description };
+    } else {
+      return null;
+    }
+  }
+
+  //DELETE (App.delete() )-> Database.deleteOne() -> collection.deleteOne()
+  async deleteOne(ISBN) {
+    if (this.collection != null) {
+      const result = await this.collection.deleteOne({ ISBN: ISBN });
       return { books: result.deletedCount };
     } else {
       return { books: 0 };
+    }
+  }
+
+  close() {
+    if (this.collection != null) {
+      this.collection.close();
     }
   }
 }
--- lab11/index.js
+++ lab11/index.js
@@ -1,62 +1,61 @@
+import CORS from "cors";
 import Express from "express";
 import Database from "./Database.js";
-import { rmSync } from "fs";
 
-const App = "Express";
-const port = "45030";
+const App = Express();
+const port = 45030;
+
+App.use(CORS());
 App.use(Express.json());
 
-
-//creating new database object
 const db = new Database();
-//Database.connect()
-db.connect("lab11", "VeruskaRivera");
+
+db.connect("lab11", "books");
 
 //PUT (App.put() )-> Database.createOne() -> collection.insertOne()
-App.put("/books/:ISBN", (req, res) => {
-  const isbn = req.params.isbn;
-
-  //request body properties
+App.put("/books/:ISBN", async (req, res) => {
+  const ISBN = req.params.ISBN;
   const title = req.body.title;
   const author = req.body.author;
   const description = req.body.description;
-
-  res.json({
-    title: title,
-    author: author,
-    description: description
-  });
+  let result = await db.createOne(ISBN, title, author, description);
+  res.json(result);
 });
 
 //GET (App.get() )-> Database.readOne() -> collection.findOne()
-App.get("/books/:ISBN", (req, res) => {
-  const isbn = req.params.isbn;
-  res.json({ book: "not found" });
+App.get("/books/:ISBN", async (req, res) => {
+  const ISBN = req.params.ISBN;
+  const result = await db.readOne(ISBN);
+  res.json(result);
 });
 
 //POST  (App.post()) -> Database.readMany() -> collection.readMany()
-App.post("/books/search");
+App.post("/books/search", (req, res) => {
+  const title = req.query.title;
+  const author = req.query.author;
+  const result = db.readMany(title, author);
+  res.json(result);
+});
 
 //PATCH(App.patch() )-> Database.updateOne() -> collection.updateOne()
-App.patch("/books/:ISBN", (req, res) => {
-  const isbn= req.params.isbn;
-
-  //request body properties
+App.patch("/books/:ISBN", async (req, res) => {
+  const ISBN = req.params.ISBN;
   const title = req.body.title;
   const author = req.body.author;
   const description = req.body.description;
-  const result = await db.updateOne(isbn, title);
+  const result = await db.updateOne(ISBN, title, author, description);
   res.json(result);
 });
 
 //DELETE (App.delete() )-> Database.deleteOne() -> collection.deleteOne()
 App.delete("/books/:ISBN", async (req, res) => {
-  const isbn = req.params.isbn;
-  const result = await db.deleteOne(isbn);
-  res.json({result});
+  const ISBN = req.params.ISBN;
+  const result = await db.deleteOne(ISBN);
+  res.json(result);
 });
 
-App.listen(port, () =>{
+db.close();
+
+App.listen(port, () => {
   console.log("Server running!");
 });
-
