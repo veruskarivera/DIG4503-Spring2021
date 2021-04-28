7b4d73f3a34327ca47d47ae585b9c1fa14900d21
“veruskarivera” <veruska@knights.ucf.edu>
2021-04-27 20:19:01 -0400

    lab12

--- lab12/client/src/App.js
+++ lab12/client/src/App.js
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className="App">
       <h1> Hi :)</h1>
+      <h5> *have your ISBN handy! </h5>
       <h2> Add Book </h2>
       <PutBook />
       <h2> Search Book </h2>
       <GetBook />
       <h2> Delete Book</h2>
       <DeleteBook />
-      <h5> *have your ISBN handy! </h5>
     </div>
   );
 }
